2629fea9f9dddbfbe27a892c2e799864e8cab132
Shane McGowan <shane.mcgowan@intercom.io>
2023-12-12 20:26:50 +0000

    ensure uses hash routing

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import { ROUTES } from './routes.js';
 import { Router } from '/core/router.js';
 
 (function() {
+  // ensure hash routing
+  alert(window.location.hash); 
+  if(!window.location.hash.startsWith('#/')) {
+    window.location = '#/';
+  }
+
   Router.navigateToUrl();
 })();
 
